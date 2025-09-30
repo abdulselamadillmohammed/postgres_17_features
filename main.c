880e9b221b8ae4609c5df071a8bc026dc89fb554
abdulselamadillmohammed <forcoding39@gmail.com>
2025-09-30 18:55:27 -0400

    watch out for file not exist issue

--- main.c
+++ main.c
@@ -9,8 +9,13 @@ int main(){
     read(2) manpage:  https://man7.org/linux/man-pages/man2/read.2.html
     */
 
-
-
     //ssize_t read()
+
+    int fd = open("samsple.txt", O_RDONLY);
+    if (fd < 0){
+        perror("open");
+        exit(1);
+    }
+
     return 0;
 }
