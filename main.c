fc9b4510404900a37e115db336285755f9d7634d
abdulselamadillmohammed <forcoding39@gmail.com>
2025-09-30 19:07:57 -0400

    add .gitignore

--- main.c
+++ main.c
@@ -9,24 +9,29 @@ int main(){
     read(2) manpage:  https://man7.org/linux/man-pages/man2/read.2.html
     */
 
-    //ssize_t read()
+    //ssize_t read(int fd, void *buf, size_t count);
+    // int fd → the file descriptor you get from open func
+    // void *buf -> the pointer to the bugger where your bytes should be stored
+    // size_t count -> the max num of bytes you want to place in buffer (type as char since it takes bytes)
 
-    int fd = open("samsple.txt", O_RDONLY);
+    // This only fails at run time!!
+    int fd = open("sample.txt", O_RDONLY);
     if (fd < 0){
         perror("open");
         exit(1);
     }
     
     char buffer[128];
-    ssize_t n = read(fd, buffer, sizeof(buffer-1));
+    //                             leave last byte fro /0
+    ssize_t n = read(fd, buffer, sizeof(buffer) -1 );
     if (n < 0){
         perror("read");
         close(fd);
         exit(1);
     }
 
-    buffer[n] = "\0";
-    printf("Read %zd bytes");
+    buffer[n] = '\0';
+    printf("Read %zd bytes. What was read: \" %s \" \n", n, buffer);
 
     close(fd);
     return 0;
