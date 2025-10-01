9554bf0ec63bde14df212ffee539631eb729c959
abdulselamadillmohammed <forcoding39@gmail.com>
2025-10-01 12:04:44 -0400

    Reorder declarations

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(){
     // void *buf -> the pointer to the bugger where your bytes should be stored
     // size_t count -> the max num of bytes you want to place in buffer (type as char since it takes bytes)
 
+    char buffer[128];
     
     // This only fails at run time!!
     int fd = open("sample.txt", O_RDONLY);
@@ -21,8 +22,6 @@ int main(){
         perror("open");
         exit(1);
     }
-    
-    char buffer[128];
     //                             leave last byte fro /0
     ssize_t n = read(fd, buffer, sizeof(buffer) -1 );
     if (n < 0){
